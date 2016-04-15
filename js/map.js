196400910ccf294eb04f8d8c9bd9ecefcf9eac35
GuillaumeS <guillaume.simler@gmail.com>
2016-04-15 13:13:54 +0200

    Add markers to the map

--- js/map.js
+++ js/map.js
@@ -14,11 +14,96 @@ It uses widely the Jquery AJAX function (http://api.jquery.com/jquery.ajax/)
 
 
 var gMap = {
-	initMap: function() {
-	  	var map = new google.maps.Map(document.getElementById('map-section'), {
-	       	center: {lat: 56.431, lng: -4.702},
-	       	zoom: 10
-		    });
+	
+		
+	// ------------------ model --------
+	model:{
+		//Map center: coordinates of Tyndrum Station
+		center: {
+			lat: 56.436425, 
+			lng: -4.711097
+		},
+
+		//Markers:
+
+		locations:[
+			{name: 'Glengarry House',
+			position:{
+				lat: 56.431270,
+				lng: -4.702900
+				}	
+			},
+
+			{name: 'Bridge of Orchy Hotel',
+			position:{
+				lat: 56.517778, 
+				lng: -4.768667
+				}	
+			},
+
+			{name: 'Ben Lui',
+			position:{
+				lat: 56.397507, 
+				lng: -4.810323 
+				}	
+			},
+
+			{name: 'Beinn Dorain',
+			position:{
+				lat: 56.502835, 
+				lng: -4.722226 
+				}	
+			},
+
+			{name: 'Beinn an Dòthaidh',
+			position:{
+				lat: 56.529969, 
+				lng: -4.714415 
+				}	
+			}
+		]
+
+	},
+
+	// ------------------ controller ------
+
+	controller:{
+	
+		getMapCenter: function(){
+			return gMap.model.center;
+		},
+
+		getMarkerData: function(){
+			return gMap.model.locations;
+		}
+	},
+
+	// ------------------ view -----------
+	
+	view:{
+		initMap: function() {
+			var map = new google.maps.Map(document.getElementById('map-section'), {
+				center: gMap.controller.getMapCenter(),
+				zoom: 10
+			});	
+
+			this.initMarker(map);
+			
+		},
+
+		initMarker: function(data) {
+
+			var locations = gMap.controller.getMarkerData();
+
+			for (var i=0; i < locations.length; i++) {
+				var marker = new google.maps.Marker({
+					position: locations[i].position,
+					map: data,
+					title: locations[i].name,
+					zIndex: i,
+				});
+			}
+		}
 	}
 }
 
