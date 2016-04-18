5e8a8962e1d10d49eef294a765dc3ca24ba512a4
GuillaumeS <guillaume.simler@gmail.com>
2016-04-18 18:50:00 +0200

    Back on track

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
+var viewModel = ko.observableArray(locations);
 
-
-var viewModel = function(data){
-	this.name = ko.observable(data.name);
-	this.type = ko.observable(data.type);
-	this.position = ko.observable(data.position);
-};
-
-
-ko.applyBindings(viewModel(locations));
+ko.applyBindings(new viewModel());
