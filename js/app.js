aac34095cb5d5b6bc6665eaec2c6041e87fbf761
GuillaumeS <guillaume.simler@gmail.com>
2016-04-19 18:22:12 +0200

    Implement search (!!!)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-var viewModel = ko.observableArray(locations);
+var viewModel = function() {
+
+	var self = this;
+
+	self.places = ko.observableArray(locations);
+
+	self.query = ko.observable('');
+
+	self.searchedPlaces = ko.computed(function() {
+
+		console.log('At least started ' + self.query())
+
+		return ko.utils.arrayFilter(self.places(), function(item) {
+			return (item.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) ||(item.type.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) ;
+        });
+	});
+};
+
+
+
+
 
 ko.applyBindings(new viewModel());
