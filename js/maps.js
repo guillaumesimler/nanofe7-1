b0e5ba7db6b8054b52282e51c2ec0001ddb35d18
GuillaumeS <guillaume.simler@gmail.com>
2016-05-06 11:22:44 +0200

    Add AJAX Request in the Infowindow

--- js/maps.js
+++ js/maps.js
@@ -3,10 +3,6 @@ var markers = [];
 var marker;
 var map;
 
-
-
-
-
 var initMap = function() {
 		
 	//Get the data for the map
@@ -29,7 +25,6 @@ var initMap = function() {
 };
 
 
-
 var Place = function(data) {
     var self = this;
 
@@ -45,13 +40,7 @@ var Place = function(data) {
 
 };
 
-var reactiveList = function(response){
-	if (response.marker().getAnimation() !== null) {
-			response.marker().setAnimation(null);
-	} else {
-	   		response.marker().setAnimation(google.maps.Animation.BOUNCE);
-	}
- };
+
 
 
 
@@ -71,12 +60,10 @@ var viewModel = function() {
 
 
 		//Insert the text of the info string
+		var infowindow = new google.maps.InfoWindow();
+		loadWiki(place, infowindow);
 		
-		var contentString = 'A great location ' + location.name;
 
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});
 
 		//Create the marker(s)
 
@@ -112,10 +99,69 @@ var viewModel = function() {
 	});
 };
 
+// 
+
+var loadWiki = function(input, infowindow) {
+
+	var Input = input.name();
+    var content;
+
+    // Load Wikipedia Article
+    var WikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + Input + '&prop=revisions&rvprop=content&format=json&callback=wikiCallback';
+    
+   
+    $.ajax({
+        url:WikiUrl,
+        dataType: "jsonp",
+        success: function(response) {
+
+
+			console.log(response);
+
+            var articleList = response[1][0];
+            var articleSum = response[2][0];
+            var articleUrl = response[3][0];
+
+            if (articleList){
+				content = '<h5> <a href="' + articleUrl + '" target="_blank">' + articleList + '</a></h5>' +
+            	'<p>' + articleSum + '</p>';
+            } else {
+
+            	content = ('<p> failed to get this specific wikipedia article about ' + Input + 
+            		'</p><p>Please try with the <a href="https://en.wikipedia.org/wiki/Munro" target="_blank">generic article about Munros</a></p>')
+            }          
+
+            infowindow.setContent(content);
+        },
+
+        error: function(response){
+            content = ('<p> failed to get this specific wikipedia article about ' + Input + 
+                '</p><p>A major problem occured with Wikipedia - please try later or contact your administrator</a></p>')
+           
+			
+            infowindow.setContent(content);
+        }
+
+       
+    });
+
+          
+
+};
+
+// 
+
+var reactiveList = function(response){
+	if (response.marker().getAnimation() !== null) {
+			response.marker().setAnimation(null);
+	} else {
+	   		response.marker().setAnimation(google.maps.Animation.BOUNCE);
+	}
+ };
+
 
 // Fallback function for Google Maps
 var failMap = function() {
 	var errorMsg = '<div><img src="images/error.jpg" alt="a picture from Ben Lui" class="img-responsive"><p>This is indeed a Munro, but you should see a Map instead. There was an error with Google Maps</p></div>'; 
 	$("#map-section").append(errorMsg);
 };
-
